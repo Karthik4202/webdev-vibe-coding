4c012c2a9bfcb7a9efc3e74ad60bad7889c9ad6c
Karthik4202 <kamunikarthik1234@gmail.com>
2025-10-29 14:48:36 +0530

    Added script.js file

---
+++ Day-16/script.js
@@ -0,0 +1,12 @@
+console.log("Introduction To Javascript")
+console.warn("This is Warning")
+console.error("This is error")
+
+var a;//Variable declaration
+
+a = 10;//Variable initialization
+
+var b = 10;
+var c = 20;
+
+console.log(b+c)
