7fa47e5dd3e606a048d8e84988ae87d1b896e39b
Karthik4202 <kamunikarthik1234@gmail.com>
2025-12-01 14:00:18 +0530

    Added script.js file

---
+++ Day-30/script.js
@@ -0,0 +1,40 @@
+let childDiv = document.querySelector(".child");
+let h2 = document.querySelector("h2");
+
+//click
+childDiv.addEventListener('click',(event) => {
+    h2.innerHTML=`${(event.type).toUpperCase()}`
+    console.log("click", event.type);
+});
+
+//dblclick
+childDiv.addEventListener("dblclick", (event) => {
+    h2.innerHTML=`${(event.type).toUpperCase()}`
+    console.log("Double Clicked", event.type);
+});
+
+//mouseenter
+let interval = 0
+childDiv.addEventListener("mouseenter", (event) => {
+    h2.innerHTML=`${(event.type).toUpperCase()}`
+    interval = setInterval(() => {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+    
+        childDiv.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }, 200);
+});
+
+//mouseleave
+childDiv.addEventListener("mouseleave",(event)=>{
+    h2.innerHTML=`${(event.type).toUpperCase()}`
+    console.log(event.type)
+    clearInterval(interval);
+})
+
+//scroll
+window.addEventListener('scroll',(event)=>{
+    console.log(event.type);
+    h2.innerHTML=`${(event.type).toUpperCase()}`
+})
